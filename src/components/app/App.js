98f6aa02abe62191c2dcf764684e536847887310
harounm93 <mahdiharoun93@gmail.com>
2021-03-10 17:45:52 +0000

    cleaned code and moved questions to new file

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,7 @@ import Profile from '../Profile/index';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useState } from 'react';
 import { Button } from '@material-ui/core';
+import questions from "../data/index"
 
 function App() {
   const { user, isAuthenticated } = useAuth0();
@@ -12,53 +13,8 @@ function App() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [showTotal, setShowTotal] = useState(false);
 
-  const questions = [
-    {
-      questionText: 'What is the capital of Spain?',
-      answerOptions: [
-        { answerText: 'London', isCorrect: false },
-        { answerText: 'Paris', isCorrect: false },
-        { answerText: 'Madrid', isCorrect: true },
-        { answerText: 'Dublin', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'Who is CEO of Tesla?',
-      answerOptions: [
-        { answerText: 'Jeff Bezos', isCorrect: false },
-        { answerText: 'Elon Musk', isCorrect: true },
-        { answerText: 'Bill Gates', isCorrect: false },
-        { answerText: 'Tony Stark', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'The playstation was created by which company?',
-      answerOptions: [
-        { answerText: 'Sony', isCorrect: true },
-        { answerText: 'Amazon', isCorrect: false },
-        { answerText: 'Samsung', isCorrect: false },
-        { answerText: 'Apple', isCorrect: false },
-      ],
-    },
-    {
-      questionText: 'How many Game of Thrones books are there?',
-      answerOptions: [
-        { answerText: '2', isCorrect: false },
-        { answerText: '3', isCorrect: false },
-        { answerText: '6', isCorrect: false },
-        { answerText: '7', isCorrect: true },
-      ],
-    },
-    {
-    questionText: 'What club does Ronaldo play for?',
-    answerOptions: [
-      { answerText: 'Chelsea', isCorrect: false },
-      { answerText: 'Arsenal', isCorrect: false },
-      { answerText: 'Madrid', isCorrect: false },
-      { answerText: 'Juventus', isCorrect: true },
-    ],
-  },
-  ];
+ // Array of Questions moved into separate file to tidy the code  
+ 
   function handleClick(isCorrect) {
     setCurrentQuestion(currentQuestion + 1);
     if (isCorrect === true) {
---
+++ src/components/data/index.js
@@ -0,0 +1,59 @@
+const questions = [
+    {
+      questionText: 'What is the capital of Spain?',
+      answerOptions: [
+        { answerText: 'London', isCorrect: false },
+        { answerText: 'Paris', isCorrect: false },
+        { answerText: 'Madrid', isCorrect: true },
+        { answerText: 'Dublin', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'Who is CEO of Tesla?',
+      answerOptions: [
+        { answerText: 'Jeff Bezos', isCorrect: false },
+        { answerText: 'Elon Musk', isCorrect: true },
+        { answerText: 'Bill Gates', isCorrect: false },
+        { answerText: 'Tony Stark', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'The playstation was created by which company?',
+      answerOptions: [
+        { answerText: 'Sony', isCorrect: true },
+        { answerText: 'Amazon', isCorrect: false },
+        { answerText: 'Samsung', isCorrect: false },
+        { answerText: 'Apple', isCorrect: false },
+      ],
+    },
+    {
+      questionText: 'How many Game of Thrones books are there?',
+      answerOptions: [
+        { answerText: '2', isCorrect: false },
+        { answerText: '3', isCorrect: false },
+        { answerText: '6', isCorrect: false },
+        { answerText: '7', isCorrect: true },
+      ],
+    },
+    {
+    questionText: 'What club does Ronaldo play for?',
+    answerOptions: [
+      { answerText: 'Chelsea', isCorrect: false },
+      { answerText: 'Arsenal', isCorrect: false },
+      { answerText: 'Madrid', isCorrect: false },
+      { answerText: 'Juventus', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'What year was the moon landing?',
+    answerOptions: [
+      { answerText: '1976', isCorrect: false },
+      { answerText: '1969', isCorrect: true },
+      { answerText: '1972', isCorrect: false },
+      { answerText: '1958', isCorrect: false },
+    ],
+  },
+  
+  ];
+
+  export default questions
